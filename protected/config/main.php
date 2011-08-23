2d912dd08ac8b91f3c209561ee666f9d1ef4220b
Abbas Mashayekh <martianboy2005@gmail.com>
2011-08-23 17:04:07 +0430

    test: Added doctrine2yii extension

--- protected/config/main.php
+++ protected/config/main.php
@@ -47,11 +47,12 @@ return array(
 			),
 		),
 		*/
+		/*
 		'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		),
 		// uncomment the following to use a MySQL database
-		/*
+		*/
 		'db'=>array(
 			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
 			'emulatePrepare' => true,
@@ -59,7 +60,6 @@ return array(
 			'password' => '',
 			'charset' => 'utf8',
 		),
-		*/
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
             'errorAction'=>'site/error',
