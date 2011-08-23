954e6dd7dd1903994c08864626e2dae06813880f
Abbas Mashayekh <martianboy2005@gmail.com>
2011-08-23 14:44:12 +0430

    updated .gitignore

--- index.php
+++ index.php
@@ -1,16 +1,13 @@
 <?php
-error_reporting(-1);
-require_once 'MsdTools/RestDataSource.php';
-require_once 'MsdTools/SmartClientInclude.php';
-require_once 'DefineClasses.php';
-require_once 'bootstrap.php';
-require_once 'Smarty/libs/Smarty.class.php';
-require_once 'MsdTools/WhereMaker.php';
-require_once 'MsdTools/Common.php';
-require_once 'MsdTools/PageResponser.php';
-$smarty = new Smarty();
-//Get Page To Redirect To From URL
-//Test
-$phpfile ='JAHAD_UI/'. $_GET['phpmodule'].'.php';
-require $phpfile;
-?>
+
+// change the following paths if necessary
+$yii=dirname(__FILE__).'/../lib/yii/framework/yii.php';
+$config=dirname(__FILE__).'/protected/config/main.php';
+
+// remove the following lines when in production mode
+defined('YII_DEBUG') or define('YII_DEBUG',true);
+// specify how many levels of call stack should be shown in each log message
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+
+require_once($yii);
+Yii::createWebApplication($config)->run();
