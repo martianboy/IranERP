5f43fa8f209395214cfa78d3d1f86ea3405ba687
Masoud Mazarei <msd.mazarei@gmail.com>
2011-09-09 21:30:44 +0430

    Human Controller 	modified:   protected/models/DbEntity.php 	modified:   protected/modules/jahad/models/Human.php 	images/Cart.png 	protected/modules/jahad/controllers/AuidunceController.php 	protected/modules/jahad/controllers/HumanController.php 	protected/modules/jahad/controllers/MagNoController.php 	protected/modules/jahad/controllers/MagazineTypeController.php 	protected/modules/jahad/controllers/NationalityController.php 	protected/modules/jahad/controllers/SizeController.php 	protected/modules/jahad/controllers/StateController.php 	protected/modules/jahad/controllers/TVRDController.php 	protected/modules/jahad/controllers/TitleController.php 	protected/modules/jahad/controllers/YearController.php 	protected/modules/jahad/views/human/

--- protected/models/DbEntity.php
+++ protected/models/DbEntity.php
@@ -133,6 +133,7 @@ class DbEntity
 			}
 		}
 	}
+	
 	public function GetByID($id)
 	{
 		return $this->getEntityManager()
--- protected/modules/jahad/models/Human.php
+++ protected/modules/jahad/models/Human.php
@@ -13,12 +13,27 @@ class Human extends DbEntity
 	 * @Column(type="string",length=50)
 	 */
 	protected $fname;
+	
+	/**
+	 * 
+	 * @scField(type="string",title="نام",DoctrineField="fname",name="FirstName")
+	 */
 	public function getFirstName(){return $this->fname;}
 	public function setFirstName($name){$this->fname=$name;}
 	/**
 	 * @Column(type="string",length=50)
 	 */
 	protected $lname;
+	/**
+	 * 
+	 * @scField(
+	 * 	type="string",
+	 * 	title="نام خانوادگی",
+	 * 	DoctrineField="lname",
+	 * 	name="LastName"
+	 * )
+	 * 
+	 */
 	public function getLastName(){return $this->lname;}
 	public function setLastName($LName){$this->lname=$LName;}
 	
@@ -26,6 +41,15 @@ class Human extends DbEntity
 	 * @Column(type="string",length=50,nullable=true)
 	 */
 	protected $NationalCode;
+	/**
+	 * 
+	 * @scField(
+	 * 	type="string",
+	 * 	title="کدملی",
+	 * 	DoctrineField="NationalCode",
+	 * 	name="NationalCode"
+	 * )
+	 */
 	public function getNationalCode(){return $this->NationalCode;}
 	public function setNationalCode($nl){$this->NationalCode=$nl;}
 	
@@ -33,6 +57,15 @@ class Human extends DbEntity
 	 * @Column(type="string",length=50,nullable=true)
 	 */
 	protected $fathername;
+	/**
+	 * 
+	 * @scField(
+	 *	type="string",
+	 * 	title="نام پدر",
+	 * 	DoctrineField="fathername",
+	 * 	name="FatherName"
+	 * )
+	 */
 	public function getFatherName(){return $this->fathername;}
 	public function setFatherName($fn){$this->fathername=$fn;}
 	
@@ -48,14 +81,54 @@ class Human extends DbEntity
 	 * @var Nationality
 	 */
 	protected $nationality;
-	public function getNationality(){return $this->nationality;}
-	public function setNationality($nl) {$this->nationality=$nl;}
+	/**
+	 * 
+	 * @scField(
+	 * 	type="string",
+	 * 	title="ملیت",
+	 * 	DoctrineField="nationality.Name",
+	 * 	name="NationalityTitle"
+	 * )
+	 */
+	public function getNationality(){return $this->nationality->getName();}
+	public function setNationality($nl) {/*$this->nationality=$nl;*/}
+	
+	/**
+	 * 
+	 * @scField(
+	 * 	type="integer",
+	 * 	hidden=true,
+	 * 	name="NationalityID"
+	 * 	)
+	 */
+	public function getNationalityID(){
+		if(isset($this->nationality))
+			return $this->nationality->getID();
+		else
+			return null;
+	}
+	public function setNationalityID($nid){
+	//Reterive Nationality Object From Data Bank
+	$nationality= new Nationality();
+	$nationality= $nationality->GetByID($nid);
+	$this->nationality=$nationality;
+	}
+	
 	
 	/**
 	 * 
 	 * @Column(type="string",length=50,nullable=true)
 	 */
 	protected $postalcode;
+	/**
+	 * 
+	 * @scField(
+	 * 	type="string",
+	 * 	title="کدپستی",
+	 * 	name="PostalCode",
+	 * 	DoctrineField="postalcode"
+	 * )
+	 */
 	public function getPostalCode(){return $this->postalcode;}
 	public function setPostalCode($pl){$this->postalcode=$pl;}
 	
