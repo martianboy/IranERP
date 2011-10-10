34092a0b0456aab16fabba8deb26b4f807be6afb
Masoud Mazarei <msd.mazarei@gmail.com>
2011-10-10 16:02:23 +0330

    Nemidunam chi begam 	new file:   protected/components/MainLayoutHelpers.php 	new file:   protected/views/layouts/column1.php 	new file:   protected/views/layouts/column2.php 	new file:   protected/views/layouts/main.php

---
+++ protected/components/MainLayoutHelpers.php
@@ -0,0 +1,36 @@
+<?php
+class MainLayoutHelpers extends CComponent
+{
+	public static function GetSmartClientJs()
+	{
+		$resources = array();
+		$BasicPath = Yii::app()->baseUrl;
+		$isomorphicDir=$BasicPath.'/isomorphic/';
+		$url = 'http://'.Yii::app()->request->getServerName();
+		$url .='/IranERP/';
+		$resources[] = '<script type="text/javascript">var baseurl="'.$url.'";</script>';
+		$resources[] = '<script type="text/javascript">var isomorphicDir="'.$isomorphicDir.'";</script>';
+		$resources[] = '<script type="text/javascript" src='.$isomorphicDir.'system/modules/ISC_History.js?isc_version=SC_SNAPSHOT-2011-01-06.js></script>';
+		$resources[] = '<script type="text/javascript" src='.$isomorphicDir.'system/development/ISC_FileLoader.js?isc_version=SC_SNAPSHOT-2011-01-06.js></script>';
+		$resources[] = '<script type="text/javascript" src='.$isomorphicDir.'system/modules/ISC_Core.js?isc_version=SC_SNAPSHOT-2011-01-06.js></script>';
+		$resources[] = '<script type="text/javascript" src='.$isomorphicDir.'system/modules/ISC_Foundation.js?isc_version=SC_SNAPSHOT-2011-01-06.js></script>';
+		$resources[] = '<script type="text/javascript" src='.$isomorphicDir.'system/modules/ISC_Containers.js?isc_version=SC_SNAPSHOT-2011-01-06.js></script>';
+		$resources[] = '<script type="text/javascript" src='.$isomorphicDir.'system/development/ISC_Grids.js?isc_version=SC_SNAPSHOT-2011-01-06.js></script>';
+		$resources[] = '<script type="text/javascript" src='.$isomorphicDir.'system/modules/ISC_Forms.js?isc_version=SC_SNAPSHOT-2011-01-06.js></script>';
+		$resources[] = '<script type="text/javascript" src='.$isomorphicDir.'system/modules/ISC_RichTextEditor.js?isc_version=SC_SNAPSHOT-2011-01-06.js></script>';
+		$resources[] = '<script type="text/javascript" src='.$isomorphicDir.'system/modules/ISC_DataBinding.js?isc_version=SC_SNAPSHOT-2011-01-06.js></script>';
+		$resources[] = '<script type="text/javascript" src='.$isomorphicDir.'skins/Graphite/load_skin.js></script>';
+		$resources[] = '<script type="text/javascript" src='.$BasicPath.'/js/GeneralFuncs.js></script>';
+		
+		/*$resources[] ='<script>';
+		$resources[] ='isc.currentSkin = isc.params.skin;';
+		$resources[] ='if (isc.currentSkin == null) isc.currentSkin = "Enterprise";';
+		$resources[] ='isc.FileLoader.loadSkin(isc.currentSkin, "showExplorer()");';
+		$resources[] ='</script>';
+		*/
+		
+		return $resources;
+	}
+}
+
+?>
